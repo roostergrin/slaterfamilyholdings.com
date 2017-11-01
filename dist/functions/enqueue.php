f59de1e57ce6fcb5b40baa05fe2dfd28b6ccb0f7
mattlaguardia <matthewlaguardia@gmail.com>
2017-11-01 16:40:09 -0700

    updates index.php

--- dist/functions/enqueue.php
+++ dist/functions/enqueue.php
@@ -1,10 +1,10 @@
 <?php
 
 function load_scripts() {
-	wp_enqueue_script( 'main-manifest', get_template_directory_uri() . '/dist/static/js/manifest.js', array(), '1.0.0', true );
-	wp_enqueue_script( 'main-vendor', get_template_directory_uri() . '/dist/static/js/vendor.js', array(), '1.0.0', true );
-	wp_enqueue_script( 'main-scripts', get_template_directory_uri() . '/dist/static/js/app.js', array(), '1.0.0', true );
-	wp_enqueue_style('main-styles', get_template_directory_uri() . '/dist/static/css/app.css', array(), '1.0.0', 'all');
+	wp_enqueue_script( 'main-manifest', get_template_directory_uri() . '/static/js/manifest.js', array(), '1.0.0', true );
+	wp_enqueue_script( 'main-vendor', get_template_directory_uri() . '/static/js/vendor.js', array(), '1.0.0', true );
+	wp_enqueue_script( 'main-scripts', get_template_directory_uri() . '/static/js/app.js', array(), '1.0.0', true );
+	wp_enqueue_style('main-styles', get_template_directory_uri() . '/static/css/app.css', array(), '1.0.0', 'all');
 
 	remove_action('wp_head', 'print_emoji_detection_script', 7);
 	remove_action('wp_print_styles', 'print_emoji_styles');
@@ -12,15 +12,15 @@ function load_scripts() {
 	remove_action( 'admin_print_styles', 'print_emoji_styles' );
 }
 add_action('wp_enqueue_scripts', 'load_scripts');
-
-function google_fonts() {
-	$query_args = array(
-		'family' => 'Roboto'
-	);
-	wp_enqueue_style( 'google_fonts', add_query_arg( $query_args, "//fonts.googleapis.com/css" ), array(), null );
-}
-
-add_action('wp_enqueue_scripts', 'google_fonts');
+//
+// function google_fonts() {
+// 	$query_args = array(
+// 		'family' => 'Roboto'
+// 	);
+// 	wp_enqueue_style( 'google_fonts', add_query_arg( $query_args, "//fonts.googleapis.com/css" ), array(), null );
+// }
+//
+// add_action('wp_enqueue_scripts', 'google_fonts');
 
 function get_menu() {
     # Change 'menu' to your own navigation slug.
