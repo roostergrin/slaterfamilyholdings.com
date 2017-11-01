d0834cee4d876047157db25555d2e8ea0893c93e
mattlaguardia <matthewlaguardia@gmail.com>
2017-11-01 13:45:34 -0700

    updates gzipping

--- functions/enqueue.php
+++ functions/enqueue.php
@@ -2,9 +2,9 @@
 
 function load_scripts() {
 	wp_enqueue_script( 'main-manifest', get_template_directory_uri() . '/dist/static/js/manifest.js', array(), '1.0.0', true );
-	wp_enqueue_script( 'main-vendor', get_template_directory_uri() . '/dist/static/js/vendor.js', array(), '1.0.0', true );
+	wp_enqueue_script( 'main-vendor', get_template_directory_uri() . '/dist/static/js/vendor.js.gz', array(), '1.0.0', true );
 	wp_enqueue_script( 'main-scripts', get_template_directory_uri() . '/dist/static/js/app.js', array(), '1.0.0', true );
-	wp_enqueue_style('main-styles', get_template_directory_uri() . '/dist/static/css/app.css', array(), '1.0.0', 'all');
+	wp_enqueue_style('main-styles', get_template_directory_uri() . '/dist/static/css/app.css.gz', array(), '1.0.0', 'all');
 }
 add_action('wp_enqueue_scripts', 'load_scripts');
 
