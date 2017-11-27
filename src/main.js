a4d8955ab5a3812936ae0459a449d3bc5f67e030
luketevers <luketevers@gmail.com>
2017-11-27 15:57:26 -0800

    added vue-waypoints and vue-scrollTo too in mainJS as well moved them and vue-parallax to dev-dependencies out of dependencies

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import * as customFilters from './shared/filters/filters'
 import store from './shared/store'
 import VueCookies from 'vue-cookies'
 // import VueParallaxJs from 'vue-parallax-js'
+// import VueScrollTo from 'vue-scrollto'
+// import VueWaypoint from 'vue-waypoint'
 
 // Vue config
 Vue.config.productionTip = false
@@ -19,7 +21,19 @@ Object.keys(customFilters).forEach(key => {
 
 // Vue use
 Vue.use(VeeValidate)
-// Vue.use(VueParallaxJs)
+// Vue.use(VueParallaxJs, { minWidth: 1024 })
+// Vue.use(VueScrollTo, {
+//   container: 'body',
+//   duration: 1500,
+//   easing: 'ease',
+//   offset: 0,
+//   cancelable: true,
+//   onDone: false,
+//   onCancel: false,
+//   x: false,
+//   y: true
+// })
+// Vue.use(VueWaypoint)
 Vue.use(VueCookies)
 
 /* eslint-disable no-new */
