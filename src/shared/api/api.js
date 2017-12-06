deec7b9e8df62a0d4e57cfe94bfc1801dd77bcbc
mattlaguardia <matthewlaguardia@gmail.com>
2017-12-06 10:54:56 -0800

    updates(build-dev.js): updates api for dev and production

--- src/shared/api/api.js
+++ src/shared/api/api.js
@@ -1,3 +1,12 @@
-let api = (document.location.protocol === 'https:' ? 'https' : 'http') + '//api.roostertest3.com/wp-json'
+const api = Api()
+
+function Api () {
+  if (process.env.NODE_ENV === 'production') {
+    return (document.location.protocol === 'http' ? 'https' : 'http') + '://api.roostertest3.com/wp-json'
+  }
+  return '//api.roostertest3.com.com/wp-json'
+}
+
+// TODO: BE SURE TO CHANGE THE RETURN AFTER LAUNCH
 
 export default api
