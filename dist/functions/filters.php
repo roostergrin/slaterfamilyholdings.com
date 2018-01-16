2beb29bc22ab471f0c2ec00e9db93b7d136d0bf5
mattlaguardia <matthewlaguardia@gmail.com>
2018-01-16 11:26:13 -0800

    feat(api-rest-cache): adds filter for new plugin

--- dist/functions/filters.php
+++ dist/functions/filters.php
@@ -14,3 +14,10 @@ add_filter( 'allow_dev_auto_core_updates', '__return_false' );
 add_filter( 'allow_minor_auto_core_updates', '__return_true' );
 add_filter( 'allow_major_auto_core_updates', '__return_true' );
 add_filter( 'auto_core_update_send_email', '__return_false' );
+
+// Rest API CACHE
+add_action( 'save_post', function( $post_id ) {
+  if ( class_exists( 'WP_REST_Cache' ) ) {
+    WP_REST_Cache::empty_cache();
+  }
+});
