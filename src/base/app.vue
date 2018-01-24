8c63db3fcebb36eeea978fcbcecacd9271ecd6a5
luketevers <luketevers@gmail.com>
2018-01-24 11:59:51 -0800

    added complete init store dispatch to app.vue

--- src/base/app.vue
+++ src/base/app.vue
@@ -8,6 +8,11 @@ export default {
   created () {
     this.$store.dispatch('GET_PAGES')
     this.$store.dispatch('GET_APP')
+    this.$store.dispatch('VIEW_NAV', false)
+    this.$store.dispatch('VIEW_BODY', false)
+    this.$store.dispatch('VIEW_TYPES', 0)
+    this.$store.dispatch('VIEW_SHOWMODAL', false)
+    this.$store.dispatch('VIEW_MODALCONTENT', false)
   },
   computed: {
     props () {
