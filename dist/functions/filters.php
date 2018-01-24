31ebc0d7ded93fcd2a8a4db0f5addc63a39441e9
mattlaguardia <matthewlaguardia@gmail.com>
2018-01-24 10:30:49 -0800

    fix(filters.php): adds the svg script

--- dist/functions/filters.php
+++ dist/functions/filters.php
@@ -14,6 +14,17 @@ add_filter( 'allow_dev_auto_core_updates', '__return_false' );
 add_filter( 'allow_minor_auto_core_updates', '__return_true' );
 add_filter( 'allow_major_auto_core_updates', '__return_true' );
 add_filter( 'auto_core_update_send_email', '__return_false' );
+add_filter('upload_mimes', 'cc_mime_types');
+add_filter('wp_mail_content_type','set_content_type');
+
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+
+function set_content_type($content_type){
+  return 'text/html';
+}
 
 // Rest API CACHE
 add_action( 'save_post', function( $post_id ) {
