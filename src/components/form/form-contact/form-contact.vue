5b3a4e3666c1b88f6388ffae80a1da726f825e37
Mike <mcheng.oc@gmail.com>
2018-08-03 10:33:50 -0700

    feat/page-contact: Update form configs

--- src/components/form/form-contact/form-contact.vue
+++ src/components/form/form-contact/form-contact.vue
@@ -1,31 +1,47 @@
-<template lang="pug" src="./form-contact.pug"></template>
+<template lang='pug' src='./form-contact.pug'></template>
 
 <script>
+import api from 'api'
+import axios from 'axios'
 export default {
-  props: ['props'],
   data: () => {
     return {
       name: '',
       email: '',
       subject: '',
       message: '',
+      modal: false,
+      postUrl: api + '/rg-mail/v1/contact',
       formSubmitted: false
     }
   },
   methods: {
-    validateBeforeSubmit () {
+    validate () {
       this.$validator.validateAll()
       .then(result => {
         if (result) {
-          this.$el.children[0].submit()
-          this.formSubmitted = true
-        } else {
-          alert('Correct them errors!')
+          this.onSubmit()
         }
       })
-      .catch(() => {
-        alert('Correct them errors!')
+      .catch((e) => {
+        console.log(e)
       })
+    },
+    onSubmit () {
+      axios.post(this.postUrl, {
+        name: this.name,
+        email: this.email,
+        subject: this.subject,
+        message: this.message
+      })
+      .then(res => {
+        this.modal = true
+        setTimeout(() => {
+          this.$el.children[0].reset()
+          this.modal = false
+        }, 1000)
+      })
+      .catch(e => { console.log(e) })
     }
   }
 }
