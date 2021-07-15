71a4d27e1f2d856009121b7c6ddb0df1351e68ea
GreenWebMedia <37212563+GreenWebMedia@users.noreply.github.com>
2021-07-15 14:38:52 +0530

    Update index.php

--- dist/index.php
+++ dist/index.php
@@ -22,5 +22,20 @@
       s.parentNode.insertBefore(wf, s);
     })();
   </script>
+  
+  <script type="text/javascript"> 
+  (function(d){
+    var s = d.createElement("script");
+    s.setAttribute("data-account", "D3656BNpyD");
+    s.setAttribute("src", "https://cdn.userway.org/widget.js");
+    (d.body || d.head).appendChild(s);})(document);
+ 
+  (function(d){
+    var ns = d.createElement("noscript");
+    ns.innerHTML = `Please ensure Javascript is enabled for purposes of <a href="https://userway.org">website accessibility</a>`;
+    (d.body || d.head).appendChild(ns);})(document);    
+</script>
+
+<div id="accessibilityWidget" tabindex="0">Click for Accessibility</div>
 </body>
 </html>
